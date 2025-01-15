dd581916d636191380e60c1bd81df77de83650b9
Peter Verveer <pieter.verveer@tno.nl>
2025-01-15 10:14:15 +0000

    Remove result_id

--- examples/discrete_ga.py
+++ examples/discrete_ga.py
@@ -57,7 +57,6 @@ def report(event: Event) -> None:
     for item in event.data["results"]:
         if isinstance(item, FunctionResults):
             assert item.functions is not None
-            print(f"evaluation: {item.result_id}")
             print(f"  variables: {item.evaluations.variables}")
             print(f"  objective: {item.functions.weighted_objective}\n")
 
@@ -72,7 +71,6 @@ def run_optimization(config: dict[str, Any]) -> None:
     )
     assert optimal_result is not None
     assert optimal_result.functions is not None
-    print(f"BEST RESULT: {optimal_result.result_id}")
     print(f"  variables: {optimal_result.evaluations.variables}")
     print(f"  objective: {optimal_result.functions.weighted_objective}\n")
 
