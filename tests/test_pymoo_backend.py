b278f2e142a56e4d11d81ee9fb8e796bf8256798
Peter Verveer <pieter.verveer@tno.nl>
2026-03-24 14:18:42 +0100

    Small fix

--- tests/test_pymoo_backend.py
+++ tests/test_pymoo_backend.py
@@ -10,8 +10,11 @@ from ropt.config import EnOptConfig
 from ropt.enums import EnOptEventType, ExitCode
 from ropt.events import EnOptEvent
 from ropt.results import FunctionResults
-from ropt.transforms import OptModelTransforms
-from ropt.transforms.base import NonLinearConstraintTransform, ObjectiveTransform
+from ropt.transforms import (
+    NonLinearConstraintTransform,
+    ObjectiveTransform,
+    OptModelTransforms,
+)
 from ropt.workflow import BasicOptimizer
 
 # ruff: noqa: FBT001
